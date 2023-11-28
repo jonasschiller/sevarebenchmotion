ff41ee1255fba838e4137f059eccc3975bbc416b
Jonas Schiller <q533604@europe.bmw.corp>
2023-11-28 15:52:35 +0100

    Exclude llvm chain installation

--- host_scripts/host_setup.sh
+++ host_scripts/host_setup.sh
@@ -35,59 +35,18 @@ echo 'unattended-upgrades unattended-upgrades/enable_auto_updates boolean false'
 export DEBIAN_FRONTEND=noninteractive
 apt update
 apt install -y automake build-essential cmake git libboost-dev libboost-thread-dev \
-    libntl-dev libsodium-dev libssl-dev libtool m4 python3 texinfo yasm linux-cpupower \
+    libntl-dev libsodium-dev libssl-dev libtool m4 texinfo yasm linux-cpupower \
     python3-pip time parted libomp-dev htop wget gnupg software-properties-common \
     lsb-release 
 pip3 install -U numpy
 checkConnection "github.com"
 echo 'deb http://deb.debian.org/debian testing main' > /etc/apt/sources.list.d/testing.list
-bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
+#bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
 apt update -y
 apt install -y gcc-12 g++-12
 git clone "$REPO" "$REPO_DIR"
 git clone "$REPO2" "$REPO2_DIR"
 
-echo ls
-echo "\n" 
-
-if ! command -v update-alternatives &> /dev/null; then
-    echo "update-alternatives command not found. This script is intended for Debian/Ubuntu-based systems."
-    exit 1
-fi
-
-# Set the priority for GCC-12 (adjust the path as needed)
-GCC_PATH="/usr/bin/gcc-12"
-GPP_PATH="/usr/bin/g++-12"
-GCC_PRIORITY=100
-GPP_PRIORITY=100
-
-# Check if GCC-12 exists
-if [ ! -f "$GCC_PATH" ]; then
-    echo "GCC-12 not found at $GCC_PATH. Please make sure it is installed."
-    exit 1
-fi
-
-# Check if G++-12 exists
-if [ ! -f "$GPP_PATH" ]; then
-    echo "g++-12 not found at $GPP_PATH. Please make sure it is installed."
-    exit 1
-fi
-
-# Register GCC-12 as an alternative to the default GCC
-update-alternatives --install /usr/bin/gcc gcc "$GCC_PATH" "$GCC_PRIORITY"
-
-# Register G++-12 as an alternative to the default C++
-update-alternatives --install /usr/bin/g++ g++ "$GPP_PATH" "$GPP_PRIORITY"
-# Select GCC-12 as the default
-update-alternatives --set gcc "$GCC_PATH"
-# Select G++-12 as the default
-update-alternatives --set g++ "$GPP_PATH"
-
-# Display the current default version
-echo "GCC-12 is now the default GCC version."
-gcc --version
-g++ --version
-echo "$(gcc --version)"
 # load custom htop config
 mkdir -p .config/htop
 cp "$REPO2_DIR"/helpers/htoprc ~/.config/htop/
