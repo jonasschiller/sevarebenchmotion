2837233a71ea5751fedd2f40fec2edb039cd49ac
Jonas Schiller <q533604@europe.bmw.corp>
2023-11-30 13:43:22 +0100

    Back to crosstabs

--- host_scripts/measurement.sh
+++ host_scripts/measurement.sh
@@ -83,7 +83,7 @@ done
 
 # run the SMC protocol
 $skip ||
-    /bin/time -f "$timerf" ./"benchmark-integers" --my-id $player --parties $ips &> "$log" || success=false
+    /bin/time -f "$timerf" ./"crosstabs" --my-id $player --parties $ips --input 1 3 5 6 9 &> "$log" || success=false
 
 pos_upload  "$log"
 
