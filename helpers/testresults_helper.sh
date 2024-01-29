98f2f4aaf283bde88f3bfa3634afa1867bfc248a
Jonas Schiller <q533604@europe.bmw.corp>
2024-01-29 10:01:37 +0100

    Changes

--- helpers/testresults_helper.sh
+++ helpers/testresults_helper.sh
@@ -61,7 +61,7 @@ exportExperimentResults() {
         
         # get pos filepath of the measurements for the current loop
         runtimeinfo=$(find "$resultpath" -name "testresults_run*$i" -print -quit)
-        if [ ! -f "$runtimeinfo" ] || [ ! -f "$compileinfo" ]; then
+        if [ ! -f "$runtimeinfo" ]; then
             styleOrange "    Skip - File not found error: runtimeinfo or compileinfo"
             continue 2
         fi
